8e706ff74924c9b0ab03c25c1b1802ae59109db7
MikiZenebe <mikizenebe10gmail.com>
2024-12-27 23:27:45 +0300

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div>
+    <Button variant="destructive"> Destructive </Button>
+  </div>
+</template>
